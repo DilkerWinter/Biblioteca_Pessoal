8a7c50c5ad15e3657933c92628740ddff5bea37f
Bruno Winter <brunohdwinter@gmail.com>
2024-11-07 18:14:30 -0400

    Test Criado

---
+++ tests/LivroControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/controllers/LivroController.php';
+
+use PHPUnit\Framework\TestCase;
+
+class LivroControllerTest extends TestCase
+{
+
+}
